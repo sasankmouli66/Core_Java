7a82265ef0c3f94a85a86c874745ab666bd5bd9d
sasankmouli <sasankmouli66@gmail.com>
2026-07-28 10:07:39 +0530

    commit "I add a file java introduction"

---
+++ my_first_javaproject/src/com/javaintro/Pencil.java
@@ -0,0 +1,29 @@
+package com.javaintro;
+
+public class Pencil {
+	//static int pencil = 100;
+
+//	void pencilInfo() {
+//		int pencilBuy = 7;
+//	    int totalpencilBuy = pencil / pencilBuy; 
+//	    System.out.println("toatl pencil buy =" + totalpencilBuy);
+//	    int Remaing = pencil % pencilBuy; 
+//	    System.out.println("Remaing Cost =" + Remaing);
+//	   
+//	}
+
+	public static void main(String[] args) {
+//		Pencil p1 = new Pencil();
+//		p1.pencilInfo();
+		
+		int pencil = 100;
+		int pencilBuy = 7;
+	    int totalpencilBuy = pencil / pencilBuy; 
+	    System.out.println("toatl pencil buy =" + totalpencilBuy);
+	    int Remaing = pencil % pencilBuy; 
+	    System.out.println("Remaing Cost =" + Remaing);
+
+		
+	}
+
+}
