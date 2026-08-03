66084298f75494334fc8b86c36b472e478833e21
sasankmouli <sasankmouli66@gmail.com>
2026-08-03 13:51:34 +0530

    commit "Data Types"

---
+++ my_first_javaproject/src/com/datatypes/DataTypes.java
@@ -0,0 +1,24 @@
+package com.datatypes;
+
+public class DataTypes {
+	int i;
+	short s;
+	byte b;
+	long l;
+	float f;
+	double d;
+	char c;
+	boolean B;
+	public static void main(String[] args) {
+		DataTypes t1 = new DataTypes();
+		System.out.println("Integer : "+ t1.i);
+		System.out.println("Short : "+ t1.s);
+		System.out.println("Byte : "+ t1.b);
+		System.out.println("Long : "+ t1.l);
+		System.out.println("Float : "+ t1.f);
+		System.out.println("Double : "+ t1.d);
+		System.out.println("Char : " + t1.c);
+		System.out.println("Boolean : "+ t1.B);
+	}
+
+}
