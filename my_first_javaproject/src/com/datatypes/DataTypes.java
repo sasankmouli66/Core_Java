c3703f8c222dbbd4109e7a052c0a84ab56d1474f
sasankmouli <sasankmouli66@gmail.com>
2026-08-05 22:09:46 +0530

    commit "Data Types"

--- my_first_javaproject/src/com/datatypes/DataTypes.java
+++ my_first_javaproject/src/com/datatypes/DataTypes.java
@@ -1,19 +1,19 @@
 package com.datatypes;
-
 public class DataTypes {
-	int i;
-	short s;
-	byte b;
-	long l;
-	float f;
-	double d;
-	char c;
-	boolean B;
+	int i = 20;  //Integer is a Numeric Values
+	byte b = (byte)128; //Eclipt Type casting is int to bye casting
+	short s = b; //Implict Type Casting : it converts to byte to short
+	long l = 568942169l; // 
+	float f = 60f;
+	double d = 26d;
+	char c = 'l';
+	boolean B = true;
 	public static void main(String[] args) {
 		DataTypes t1 = new DataTypes();
 		System.out.println("Integer : "+ t1.i);
 		System.out.println("Short : "+ t1.s);
 		System.out.println("Byte : "+ t1.b);
+		System.out.println("Short : "+ t1.s);
 		System.out.println("Long : "+ t1.l);
 		System.out.println("Float : "+ t1.f);
 		System.out.println("Double : "+ t1.d);
