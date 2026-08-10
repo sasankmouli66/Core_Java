0761dba0da5fd0c454176e6396c537f80043c7e7
sasankmouli <sasankmouli66@gmail.com>
2026-08-10 18:40:33 +0530

    commit "Data Types"

---
+++ my_first_javaproject/src/com/datatypes/DataTypes2.java
@@ -0,0 +1,38 @@
+package com.datatypes;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.math.BigDecimal;
+
+public class DataTypes2 {
+	class dog {
+
+	}
+
+//Normal object Data Types
+	BigInteger B;
+	BigDecimal D;
+	String S;
+	StringBuffer A;
+	Arrays c;
+
+//Wrapper object data types
+	Integer a;
+	Short b;
+	Byte d;
+	Float e;
+	Double f;
+
+	public static void main(String[] args) {
+		DataTypes2  t = new DataTypes2();
+		System.out.println(t.B);
+		System.out.println(t.D);
+		System.out.println(t.S);
+		System.out.println(t.A);
+		System.out.println(t.c);
+		System.out.println(t.a);
+		System.out.println(t.f);
+
+	}
+
+}
